b306e479f22fdb934dd64d6ccaeef95e540eca07
Pierre <fritsch@puzzle.ch>
2012-12-01 17:09:55 +0100

    404 html 2 slim

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,6 +1,7 @@
 description "WAC Compass extension based on Susy and HTML5 Boilerplate"
 
 stylesheet 'screen.scss', :media => "screen, projection"
+stylesheet 'print.scss', :media => "print"
 stylesheet '_variable.scss'
 
 stylesheet '_base.scss'
@@ -18,12 +19,14 @@ stylesheet 'main/_footer.scss'
 stylesheet 'main/_header.scss'
 stylesheet 'main/_menus.scss'
 
-html '404.html'
+html '404.html.slim'
 html '_header.html.slim'
 html '_layout.html.slim'
 html '_navigation.html.slim'
 html '_sidebar.html.slim'
+html '_template.html.slim'
 html 'index.html.slim'
+html 'login.html.slim'
 
 image 'apple-touch-icon.png'
 image 'apple-touch-icon-57x57-precomposed.png'
@@ -36,3 +39,6 @@ javascript 'js/vendor/jquery-1.8.2.min.js', :to => 'vendor/jquery-1.8.2.min.js'
 javascript 'js/vendor/modernizr-2.6.2.min.js', :to => 'vendor/modernizr-2.6.2.min.js'
 javascript 'js/plugins.js', :to => 'plugins.js'
 javascript 'js/main.js', :to => 'main.js'
+
+file 'checklist.md'
+file 'humans.txt'
